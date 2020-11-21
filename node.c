cac583a10a82f98e4ea3ad25ea9d95072ee5e933
Simon Laursen <simonlaursen@protonmail.com>
2020-11-21 09:51:58 +0100

    Node network is created. Functions to navigate between the links have been created.

--- node.c
+++ node.c
@@ -1,59 +1,75 @@
-#include <stdlib.h>
 #include "node.h"
-#include "datatypes.h"
-#include "variables.h"
 #include <string.h>
-
+#include <stdio.h>
 #define RIGHT 1
-#define LEFT -1
-#define FORWARD 0
+#define LEFT 5
+#define FORWARD 3
+
+/* The contents of this function are predefined. Make changes to how network here. */
+void build_network(link* links, node* n){
+    int a[25], b[25], c[25], d[25], e[25], f[25], g[25], h[25], j[25], k[25], l[25], m[25], o[25], p[25], q[25], r[25];
+    int* A[25] ={a, b, c, d, e, f, g, h, j, k, l, m, o, p, q, r};
+    int i;
+
+    for(i = 0; i < 8; i++){
+        links[i].id = 0;
+        links[i].road = A[i];
+        links[i].len = 25;
+    }
+    construct_node(n, 0, "TEST_NODE_1",0,3,4,7,2,5,6,1);
+
+    for(i = 8; i < 16; i++){
+        links[i].id = 0;
+        links[i].road = A[i];
+        links[i].len = 25;
+    }
+    construct_node(n, 1, "TEST_NODE_2",3,11,12,4,10,13,14,9);
+
+}
 
 /* TODO: This should be cleaned up */
 void construct_node(node *n, int id, char *name, int primary1_enter, int primary1_exit, int primary2_enter,
                     int primary2_exit, int secondary1_enter, int secondary1_exit, int secondary2_enter,
                     int secondary2_exit) {
-    n->id = id;
-    strcpy(n->name, name);
-    n->links[0] = primary1_enter;
-    n->links[1] = secondary2_exit;
-    n->links[2] = secondary1_enter;
-    n->links[3] = primary1_exit;
-    n->links[4] = primary2_enter;
-    n->links[5] = secondary1_exit;
-    n->links[6] = secondary2_enter;
-    n->links[7] = primary2_exit;
+    (n+id)->id = id;
+    strcpy(n[id].name, name);
+    n[id].links[0] = primary1_enter;
+    n[id].links[1] = secondary2_exit;
+    n[id].links[2] = secondary1_enter;
+    n[id].links[3] = primary1_exit;
+    n[id].links[4] = primary2_enter;
+    n[id].links[5] = secondary1_exit;
+    n[id].links[6] = secondary2_enter;
+    n[id].links[7] = primary2_exit;
 }
 
-void add_node_to_matrix(node *n, matrix *m) {
+int continues(int out, int in, node *n) {
+    int idx_in = link_index_of(in, n), idx_out = link_index_of(out, n);
+    if(idx_in == -1 || idx_out == -1 || (idx_in + idx_out) % 7 == 0)
+        return 0;
+    else
+        return !(out % 2);
+}
+
+int link_index_of(int id, node *n) {
     int i;
-    for (i = 0; i < 8; i+=2) {
-        m->grid[n->links[i]][n->links[(i+3) % 7]] = FORWARD;
-        m->grid[n->links[i]][n->links[(i+1) % 7]] = RIGHT;
-        m->grid[n->links[i]][n->links[(i+5) % 7]] = LEFT;
-    }
-}
-
-int right_turn(int lnk_id, matrix *m) {
-    return search_matrix(lnk_id, RIGHT, m);
-}
-
-int left_turn(int lnk_id, matrix *m) {
-    return search_matrix(lnk_id, LEFT, m);
-}
-
-int forward(int lnk_id, matrix *m) {
-    return search_matrix(lnk_id, FORWARD, m);
-}
-
-matrix nodes(){
-    matrix m = {AMOUNT_LINKS, AMOUNT_LINKS};
-    m.grid[0][1] = RIGHT; /* More than half the array slots are never used. */
-}
-
-int search_matrix(int lnk_id, int dir, matrix *m) {
-    int i;
-    for (i = dir + 1; i < m->size_y; ++i) {
-        if(m->grid[lnk_id][i] == dir)
+    for (i = 0; i < 8; ++i) {
+        if(n->links[i] == id)
             return i;
     }
+    return -1;
 }
+
+int right_turn(int lnk_id, node *n) {
+    return n->links[(link_index_of(lnk_id, n) + RIGHT) % 7];
+}
+
+int left_turn(int lnk_id, node *n) {
+    int res = (link_index_of(lnk_id, n) + LEFT) % 7;
+    return 0 ? n->links[7] : n->links[res];
+}
+
+int forward(int lnk_id, node *n) {
+    return n->links[(link_index_of(lnk_id, n) + FORWARD) % 7];
+}
+
--- node.h
+++ node.h
@@ -1,5 +1,14 @@
 #pragma once
 #include "datatypes.h"
+
+void build_network(link* links, node* n);
+
+/* Does out continue in? I.e can you drive from in to out? */
+int continues(int out, int in, node *n);
+
+/* Returns the internal index of the id in the node*/
+int link_index_of(int id, node* n);
+
 /* Returns the id of the link that is the next turn in direction dir. */
 int search_matrix(int lnk_id, int dir, matrix* m);
 
@@ -12,10 +21,10 @@ void construct_node(node* n, int id, char* name, int primary1_enter, int primary
 void add_node_to_matrix(node* n, matrix* m);
 
 /* Given a road, returns the left road at the next node*/
-int left_turn(int lnk_id, matrix *m);
+int left_turn(int lnk_id, node *n);
 
-/* Given a road, returns the left road at the next node*/
-int right_turn(int lnk_id, matrix *m);
+/* Given a road, returns the right road at the next node*/
+int right_turn(int lnk_id, node *n);
 
-/* Given a road, returns the left road at the next node*/
-int forward(int lnk_id, matrix *m);
+/* Given a road, returns the forward road at the next node*/
+int forward(int lnk_id, node *n);
