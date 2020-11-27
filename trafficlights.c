ce4a58ecacfa3efe1121c4b6c4eab1218a59cf21
MJHC <magnus@bonusfamilien.dk>
2020-11-27 10:50:14 +0100

    Added traffic light control

---
+++ trafficlights.c
@@ -0,0 +1,50 @@
+//
+// Created by Magnus on 27-11-2020.
+//
+#include "trafficlights.h"
+#include "variables.h"
+#include "datatypes.h"
+
+#define RADIUS 10
+
+
+enum light_state {Red, Green};
+
+/*Checks if the plusbus is near intersection*/
+int check_plusbus(int r, link link, vehicle *actor){
+    int i, run = 0, radius;
+    radius = link.len - r;
+    for(i = 1; i <= AMOUNT_VEHICLES; i++){
+        if(actor[i].is_plusbus == 1 && link.road[radius] == actor[i].id)
+            run = 1;
+    }
+    return run;
+}
+
+/*changes traffic lights from green to red and vise versa*/
+void change_lights(node *node, vehicle *actor, link *link){
+    int id, count = 0;
+    /*Checks if the plusbus is in radius for prioritization*/
+    if(check_plusbus(RADIUS, *link, actor) == 1 && node.state == Green && counter < RADIUS){
+        count += 5;
+    }else if(check_plusbus(RADIUS, *link, actor) == 1 && node.state == Red){
+        count = 5;
+    }
+
+    /*How much time before it should be red/green*/
+    if(count == 10 && node.state == Red){
+        count = 0;
+        node.state = Green;
+    }else if(count == 10 && node.state == Green){
+        count = 0;
+        node.state = Red;
+    }
+
+    /*if its red then stop the last car*/
+    if(node.state = Red){
+        id = link->road[link->len-1];
+        actor[id].v = 0;
+    }
+
+    count++;
+}
---
+++ trafficlights.h
@@ -0,0 +1,2 @@
+void change_lights(node *node, vehicle *actor, link *link);
+int check_plusbus(int r, link link, vehicle *actor);
