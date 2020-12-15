f58a2828877a8d90595005cf9fc250520282cd38
MJHC <magnus@bonusfamilien.dk>
2020-12-15 13:03:24 +0100

    Added Comments. HARDCODED SOLUTION ON LINE 84 IN trafficlights.c

--- trafficlights.c
+++ trafficlights.c
@@ -4,8 +4,13 @@ void link_stop(link *link, vehicle *vehicle) {
   int car_found = 0;
   int i = 2, l, p;
 
+  /*Runs if there isn't a vehicle in the last cell of a link*/
   if (link->road[link->len - 1] == 0) {
     while (!car_found) {
+      /*
+       * Searches for a car in a radius from 2-6 away from the last cell
+       * sets car_found to 1 if a car has been found
+       * */
       if (i <= 6) {
         l = link->road[link->len - i];
         if (l != 0) {
@@ -18,13 +23,14 @@ void link_stop(link *link, vehicle *vehicle) {
       i++;
     }
   } else {
+    /*else it stops the car in the last cell*/
     p = link->road[link->len - 1];
     vehicle[p - 1].v = 0;
   }
 
 }
 
-int check_plusbus(vehicle *vehicle, link *link) {
+int check_plusbus(link *link, vehicle *vehicle) {
   int run = 0, l, plusbus_found = 0, i = 1;
 
   while (!plusbus_found) {
@@ -43,8 +49,8 @@ int check_plusbus(vehicle *vehicle, link *link) {
   return run;
 }
 
-void prioritize_plusbus(vehicle *vehicle, link *link) {
-  int pb = check_plusbus(vehicle, link);
+void prioritize_plusbus(link *link, vehicle *vehicle) {
+  int pb = check_plusbus(link, vehicle);
   switch (link->intersection->type) {
     case 'c':
       if (pb == 1 && link->intersection->layout.type_c.data.state == Green && link->intersection->layout.type_c.data.counter < PLUSBUS_R && link->intersection->layout.type_c.data.counter != 0) {
@@ -72,7 +78,7 @@ void prioritize_plusbus(vehicle *vehicle, link *link) {
   }
 }
 
-
+/*HARDCODED INTERSECTION AMOUNT*/
 void change_state(intersection *intersection) {
   int i;
   for (i = 0; i < 9; i++) {
@@ -136,10 +142,9 @@ light_data* i_data(link *link) {
 }
 
 int traffic_light(link *link, vehicle *vehicle) {
-  /*printf("test counter %d \n", i_data(link)->counter);*/
   switch (link->intersection->type) {
     case 'c':
-      return intersection_traffic_lights_type_c(vehicle, link,
+      return intersection_control_type_c(vehicle, link,
                                                 link->intersection->layout.type_c.links[10]->id,
                                                 link->intersection->layout.type_c.links[8]->id,
                                                 link->intersection->layout.type_c.links[0]->id,
@@ -147,13 +152,13 @@ int traffic_light(link *link, vehicle *vehicle) {
                                                 link->intersection->layout.type_c.links[2]->id,
                                                 link->intersection->layout.type_c.links[6]->id);
     case 'd':
-      return intersection_traffic_lights_type_d(vehicle, link,
+      return intersection_control_type_d(vehicle, link,
                                                 link->intersection->layout.type_d.links[7]->id,
                                                 link->intersection->layout.type_d.links[1]->id,
                                                 link->intersection->layout.type_d.links[5]->id,
                                                 link->intersection->layout.type_d.links[3]->id);
     case 'e':
-      return intersection_traffic_lights_type_e(vehicle, link,
+      return intersection_control_type_e(vehicle, link,
                                                 link->intersection->layout.type_e.links[6]->id,
                                                 link->intersection->layout.type_e.links[8]->id,
                                                 link->intersection->layout.type_e.links[2]->id,
@@ -161,62 +166,64 @@ int traffic_light(link *link, vehicle *vehicle) {
                                                 link->intersection->layout.type_e.links[4]->id);
 
     default:
-      return Green;
+      return 1;
   }
 }
 
-int intersection_traffic_lights_type_c(vehicle *vehicle, link *link,
+int intersection_control_type_c(vehicle *vehicle, link *link,
                               int pb_e1, int pb_e2,int l_e1,int l_e2,int l_e3,int l_e4) {
   if (link->intersection->layout.type_c.data.state == Red) {
+    /*Runs if current link is one of these roads*/
     if (link->id == pb_e1 || link->id == pb_e2 || link->id == l_e1 || link->id == l_e2) {
+      /*Runs if current link is a plusbus link*/
       if (link->id == pb_e1 || link->id == pb_e2)
-        prioritize_plusbus(vehicle, link);
+        prioritize_plusbus(link, vehicle);
       link_stop(link, vehicle);
-      return Red;
+      return 0;
 
     }
   } else {
     if (link->id == l_e3 || link->id == l_e4) {
       link_stop(link, vehicle);
-      return Red;
+      return 0;
     }
   }
-  return Green;
+  return 1;
 }
 
-int intersection_traffic_lights_type_d(vehicle *vehicle, link *link,
+int intersection_control_type_d(vehicle *vehicle, link *link,
                                         int pb_e, int l_e1, int l_e2, int l_e3) {
   if (link->intersection->layout.type_d.data.state == Red) {
     if (link->id == pb_e || link->id == l_e1 || link->id == l_e2) {
       if (link->id == pb_e)
-        prioritize_plusbus(vehicle, link);
+        prioritize_plusbus(link, vehicle);
       link_stop(link, vehicle);
-      return Red;
+      return 0;
     }
   } else {
     if (link->id == l_e3) {
-      prioritize_plusbus(vehicle, link);
+      prioritize_plusbus(link, vehicle);
       link_stop(link, vehicle);
-      return Red;
+      return 0;
     }
   }
-  return Green;
+  return 1;
 }
 
-int intersection_traffic_lights_type_e(vehicle *vehicle, link *link,
+int intersection_control_type_e(vehicle *vehicle, link *link,
                                         int pb_e, int l_e1, int l_e2,int l_e3,int l_e4) {
   if (link->intersection->layout.type_e.data.state == Red) {
     if (link->id == pb_e || link->id == l_e1 || link->id == l_e2) {
       if (link->id == pb_e || link->id == l_e1)
-        prioritize_plusbus(vehicle, link);
+        prioritize_plusbus(link, vehicle);
       link_stop(link, vehicle);
-      return Red;
+      return 0;
     }
   } else {
     if (link->id == l_e3 || link->id == l_e4) {
       link_stop(link, vehicle);
-      return Red;
+      return 0;
     }
   }
-  return Green;
+  return 1;
 }
--- trafficlights.h
+++ trafficlights.h
@@ -2,26 +2,26 @@
 #include "debug.h"
 
 /*Checks if the plusbus is near intersection*/
-int check_plusbus(vehicle *vehicle, link *link);
+int check_plusbus(link *link, vehicle *vehicle);
 
-/* Prioritizes plusbus on current lane in the traffic light */
-void prioritize_plusbus(vehicle *vehicle, link *link);
+/*Prioritizes plusbus on current lane*/
+void prioritize_plusbus(link *link, vehicle *vehicle);
 
-/*Changes lights green/red and resets counter*/
+/*Changes lights green/red and resets counter for all intersections*/
 void change_state(intersection *intersection);
 
 /*returns state of selected intersection*/
 light_data* i_data(link *link);
 
-/*runs correct traffic light control */
+/*Runs intersection control from type*/
 int traffic_light(link *link, vehicle *vehicle);
 
-/*stops car on link*/
+/*Stops car on link*/
 void link_stop(link *link, vehicle *vehicle);
 
-/*Controls traffic lights of type c intersections*/
-int intersection_traffic_lights_type_c(vehicle *vehicle, link *link, int pb_e1, int pb_e2,int l_e1,int l_e2,int l_e3,int l_e4);
-/*Controls traffic lights of type d intersections*/
-int intersection_traffic_lights_type_d(vehicle *vehicle, link *link, int pb_e, int l_e1, int l_e2, int l_e3);
-/*Controls traffic lights of type e intersections*/
-int intersection_traffic_lights_type_e(vehicle *vehicle, link *link, int pb_e, int l_e1,int l_e2,int l_e3,int l_e4);
+/*Stops vehicles on correct road in type c intersection*/
+int intersection_control_type_c(vehicle *vehicle, link *link, int pb_e1, int pb_e2,int l_e1,int l_e2,int l_e3,int l_e4);
+/*Stops vehicles on correct road in type d intersection*/
+int intersection_control_type_d(vehicle *vehicle, link *link, int pb_e, int l_e1, int l_e2, int l_e3);
+/*Stops vehicles on correct road in type e intersection*/
+int intersection_control_type_e(vehicle *vehicle, link *link, int pb_e, int l_e1,int l_e2,int l_e3,int l_e4);
